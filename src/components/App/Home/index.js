893919e9feb141f40074166454cb4b08fea56404
John Tucker <jtucker@appirio.com>
2018-12-08 08:33:25 -0500

    Image

--- src/components/App/Home/index.js
+++ src/components/App/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Page from '../../Page';
+import cat from './cat.jpg';
+import styles from './styles.module.css';
 
 const Home = () => (
   <Page>
-    Home
+    <div id={styles.rootBody}>
+      <img id={styles.rootBodyImage} alt="cat" src={cat} />
+    </div>
   </Page>
 );
 
